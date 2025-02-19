c411e2533afbe0da0807bf737d30bb80a13104ff
SivaSree-01 <sivasree2909@gmail.com>
2025-02-19 17:38:30 +0530

    jstask2

--- CA.js
+++ CA.js
@@ -1,31 +1,26 @@
 //1
 var processNumbers = function(num1, num2, callback){
     return callback(num1, num2);
-};
+}
 var result = processNumbers(3, 4, function(x, y){
     return x + y;
-});
-console.log("Sum: " + result);
-  
+})
+console.log("Sum: " + result); 
+
+
 //2
 var greet = function(callback){
     return callback("Alice");
-};
-var result;
-greet(function(name){
-    result = "Hello, " + name + "!";
-});
+}
+var result = greet(function(name) {
+    return "Hello, " + name + "!";
+})
 console.log(result);
+  
 
 //3
 var calculate = function(num1, num2, callback){
     return callback(num1, num2);
-};
-var result;
-calculate(10, 5, function(x, y) {
-    result = x - y;
-});
+}
+var result = calculate(10, 5, (x, y) => x - y);
 console.log("Difference: " + result);
-  
-  
-
