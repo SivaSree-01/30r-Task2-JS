172b82552b33594563d90314e44008f33a7f8a62
SivaSree-01 <sivasree2909@gmail.com>
2025-02-19 18:14:29 +0530

    task2

--- CA.js
+++ CA.js
@@ -2,25 +2,28 @@
 var processNumbers = function(num1, num2, callback){
     return callback(num1, num2);
 }
-var result = processNumbers(3, 4, function(x, y){
-    return x + y;
+var result = processNumbers(3, 4, function(a, b){
+    return a + b;
 })
 console.log("Sum: " + result); 
 
 
 //2
-var greet = function(callback){
+function greet(callback){
     return callback("Alice");
 }
-var result = greet(function(name) {
+var result = greet(function(name){
     return "Hello, " + name + "!";
 })
 console.log(result);
   
 
 //3
-var calculate = function(num1, num2, callback){
-    return callback(num1, num2);
+function calculate(x, y, callback){
+    return callback(x, y);
 }
-var result = calculate(10, 5, (x, y) => x - y);
+var result = calculate(10, 5, function(x, y) {
+    return x - y;
+})
 console.log("Difference: " + result);
+  
